35f3750fbd46e3fb023ffa0a40bc41ff3088edf6
sanskar5686 <88050679+sanskar5686@users.noreply.github.com>
2024-06-08 23:11:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
 
+const express = require('express'); 
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+app.use(cors(
+    {
+      origin:["https://deploy-mern-1whq.versel.app"],
+        methods:["POST","GET"],
+        credentials: true
+    }
+));
+mongoose
+    .connect('mongodb://0.0.0.0:27017/review_app')
+    .then(() => {
+        console.log('db is connected!')
+    })
+    .catch((ex) => {
+        console.log('db connection failed: ', ex)
+    })
+    const PORT = 8000;
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
